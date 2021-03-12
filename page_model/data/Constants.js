bda019c826bf6f888dbb9e50b80b6863882a97a2
Cesar CORNEJO <cesar.cornejo@geodis.com>
2021-03-12 15:47:02 -0600

    Add more credentials, fix env username override windows

--- page_model/data/Constants.js
+++ page_model/data/Constants.js
@@ -4,11 +4,23 @@ dotenv.config()
 
 export const CREDENTIALS = {
     VALID_USER: {
-        USERNAME: process.env.USER,
+        USERNAME: process.env.VALIDUSER,
         PASSWORD: process.env.PASSWORD
     },
     INVALID_USER: {
         USERNAME: 'invalid_user',
         PASSWORD: 'invalid_password'
+    },
+    LOCKED_OUT_USER: {
+        USERNAME: 'locked_out_user',
+        PASSWORD: process.env.PASSWORD
+    },
+    PROBLEM_USER: {
+        USERNAME: 'problem_user',
+        PASSWORD: process.env.PASSWORD
+    },
+    PERFORMANCE_GLITCH_USER: {
+        USERNAME: 'performance_glitch_user',
+        PASSWORD: process.env.PASSWORD
     }
 }
