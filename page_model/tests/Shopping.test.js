5242e9d8dbf28ec95197371012feab5264a02a71
Cesar CORNEJO <cesar.cornejo@geodis.com>
2021-03-13 03:09:31 -0600

    Fix add 1 item test with new functions

--- page_model/tests/Shopping.test.js
+++ page_model/tests/Shopping.test.js
@@ -19,30 +19,20 @@ test('Users can navigate to shopping cart page', async t => {
 })
 
 test('Users can add 1 item to shopping cart', async t => {
-    // Destructuring assignment, it can be merged with smart assertion
-    const [{ name: productName, addToCartBtn }] = ProductsPage.productList
-    const [{ name: cartItemName }] = ShoppingCartPage.itemList
-
-    // Smart Assertions to be calculated later
-    const productNameAssertion = productName.innerText
-    const cartItemNameAssertion = cartItemName.innerText
-
-    // Test steps after useRole
-    const productNameText = await productNameAssertion
+    const productNames = await ProductsPage.addProducts([0])
 
     await t
-        .click(addToCartBtn)
-        .expect(ProductsPage.header.shoppingCart.badge.innerText).eql('1')
         .click(ProductsPage.header.shoppingCart.container)
-        .expect(cartItemNameAssertion).eql(productNameText)
+
+    await ShoppingCartPage.checkCartItems(productNames)
 })
 
 test('Users can add multiple items to shopping cart', async t => {
     const productNames = await ProductsPage.addProducts([1, 4])
 
     await t
-        .click(ProductsPage.header.shoppingCart.container)    
-    
+        .click(ProductsPage.header.shoppingCart.container)
+
     await ShoppingCartPage.checkCartItems(productNames)
 })
 
@@ -76,11 +66,11 @@ test('User can see correct items at checkout overview', async t => {
     await t
         .click(ProductsPage.header.shoppingCart.container)
         .click(ShoppingCartPage.checkoutBtn)
-    
+
     await InformationPage.submitInformationForm(contact)
 
     await t
         .expect(OverviewPage.pageTitle.exists).ok()
-    
+
     await OverviewPage.checkCartItems(productNames)
 })
